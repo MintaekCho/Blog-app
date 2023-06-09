47599c0c3deaefb366521c848ce3cb5a8e5dd69e
Mintaek_CHO <mintaek5555@gmail.com>
2023-06-09 13:44:44 +0900

    styles: 프로필 이미지 크기 수정

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 export default function Profile() {
     return (
         <article className='flex flex-col items-center justify-center gap-2'>
-            <Image className='rounded-full' src={'/images/profile/profile.jpeg'} alt={'profile'} width={250} height={250} priority/>
+            <Image className='w-60 h-60 rounded-full' src={'/images/profile/profile.jpeg'} alt={'profile'} width={250} height={250} priority/>
             <h2 className='text-3xl font-bold'>{"Hi, I'm Mintaek"}</h2>
             <p className='text-xl font-semibold'>Front-end Engineer</p>
             <p>트러블슈팅을 좋아하는 개발자입니다.</p>
