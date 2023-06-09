5c1af2b00f7a91172e785a6c69013e95f5218720
Mintaek_CHO <mintaek5555@gmail.com>
2023-06-09 13:42:45 +0900

    styles: PostDetailPage 스타일링, footer

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,5 +1,8 @@
 import { getPostContent } from "@/api/posts";
+import MarkdownViewer from "@/components/MarkdownViewer";
+import Image from "next/image";
 import React from "react";
+import { BsCalendarDate } from "react-icons/bs";
 
 type Props = {
   params: {
@@ -7,13 +10,30 @@ type Props = {
   };
 };
 
-export default async function PostDetailPage({ params: {slug} }: Props) {
-    const post = await getPostContent({slug});
+export default async function PostDetailPage({ params: { slug } }: Props) {
+  const { title, description, content, date, path } = await getPostContent({
+    slug,
+  });
   return (
-    <div>
-      <pre>{post.title}</pre>
-      <pre>{post.description}</pre>
-      <pre>{post.content}</pre>
-    </div>
+    <article className="rounded-2xl overflow-hidden shadow-lg m-4">
+      <Image
+        className="w-full h-1/5 max-h-[500px]"
+        src={`/images/posts/${path}.png`}
+        alt={title}
+        width={760}
+        height={420}
+      />
+
+      <section className="flex flex-col gap-2 p-4">
+        <div className="flex gap-1 items-center text-lg text-sky-500 self-end font-semibold">
+          <BsCalendarDate />
+          {date.toString()}
+        </div>
+        <h3 className="text-4xl font-bold">{title}</h3>
+        <p className="text-xl font-semibold">{description}</p>
+        <div className="w-44 border-b-4 border-sky-500 my-4" />
+        <MarkdownViewer content={content} />
+      </section>
+    </article>
   );
 }
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import Link from "next/link";
+import React from "react";
+import { BsInstagram, BsGithub } from "react-icons/bs";
 
 export default function Footer() {
-    return (
-        <footer className='bg-black w-full text-white text-center'>
-            footer
-        </footer>
-    );
-}
+  return (
+    <footer className="w-full flex flex-col items-center justify-center gap-20 mt-10 p-8">
+      <div className="flex gap-8">
+        <div className="text-5xl">
+          <Link href={""}>
+            <BsInstagram />
+          </Link>
+        </div>
+        <div className="text-5xl">
+          <Link href={"https://github.com/MintaekCho"}>
+            <BsGithub />
+          </Link>
+        </div>
+      </div>
 
+      <div className="flex flex-col items-center justify-center  text-gray-400">
+        <p className="text-sm md:text-md lg:text-lg font-bold">
+          ☎ : 010-9446-3591
+        </p>
+        <p className="text-sm md:text-md lg:text-lg font-bold">
+          📮 : mintaek5555@gmail.com
+        </p>
+      </div>
+    </footer>
+  );
+}
