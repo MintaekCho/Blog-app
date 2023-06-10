313208a8a4b22c85c9bc814ab066c0e1c765bde0
Mintaek_CHO <mintaek5555@gmail.com>
2023-06-10 12:20:50 +0900

    styles: contact page 패딩값 수정

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function ContactPage() {
   return (
-    <section className="flex flex-col gap-4 items-center">
+    <section className="flex flex-col gap-4 items-center p-4">
       <div className="text-center">
         <h1 className="text-3xl font-bold">Contact</h1>
         <p className="text-lg font-semibold">mintaek5555@gmail.com</p>
